3a8ee10dbb82b1e6562ff79eaa487fe51d1b2f42
Rauch, Matthias <matthias.rauch@rubicon.eu>
2022-12-11 21:52:08 +0100

    copy existing Angular App to new Web project

--- src/Web/ClientApp/src/app/app.component.ts
+++ src/Web/ClientApp/src/app/app.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `
+    <mat-toolbar color="primary">
+      <img src="/assets/brofixe.png" alt="" class="logo"/>
+      <button mat-icon-button [matMenuTriggerFor]="menu">
+        <mat-icon>menu</mat-icon>
+      </button>
+      <span>{{title}}</span>
+      <span class="spacer"></span>
+      <button mat-icon-button>
+        <mat-icon>volunteer_activism</mat-icon>
+      </button>
+      <button mat-icon-button>
+        <mat-icon>account_circle</mat-icon>
+      </button>
+    </mat-toolbar>
+    <mat-menu #menu="matMenu">
+      <button *ngFor="let menuItem of menuItems" mat-menu-item
+              [routerLink]="menuItem.link" (click)="onMenuItemClick(menuItem.title)">
+        {{menuItem.title}}
+      </button>
+    </mat-menu>
+    <router-outlet></router-outlet>
+  `,
+  styles: [`
+    .logo {
+      height: 80%;
+    }
+
+    .spacer {
+      flex: 1 1 auto;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app';
+  menuItems: MenuItem[] = [
+    {title: "Events", link: "/events"},
+    {title: "Organisator", link: "/organizer"},
+    {title: "Admin", link: "/admin"},
+  ];
+  title = 'Events';
+
+  onMenuItemClick(title: string) {
+    this.title = title;
+  }
+}
+
+interface MenuItem {
+  title: string,
+  link: string
 }
--- src/Web/ClientApp/src/app/app.module.ts
+++ src/Web/ClientApp/src/app/app.module.ts
@@ -1,34 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {AdminComponent} from './admin/admin.component';
+import {EventsModule} from "./events/events.module";
+import {MocksModule} from "./mocks/mocks.module";
+import {OrganizerModule} from "./organizer/organizer.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    AdminComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    EventsModule,
+    OrganizerModule,
+    MocksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
