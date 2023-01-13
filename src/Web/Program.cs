3fab85c12e8f33fa122016fc7999326794d18da1
Rauch, Matthias <matthias.rauch@rubicon.eu>
2023-01-13 23:06:24 +0100

    remove unused code & package references

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -64,15 +64,6 @@ try
         pattern: "{controller}/{action=Index}/{id?}");
 
     app.MapFallbackToFile("index.html");
-    // if (app.Environment.IsDevelopment())
-    //     app.UseSpa(
-    //         spa =>
-    //         {
-    //             spa.Options.SourcePath = "ClientApp";
-    //             spa.UseProxyToSpaDevelopmentServer("https://localhost:4500");
-    //         });
-    // else
-    //     app.UseEssentialsSpaStaticFiles(env);
 
     Log.Information("Applying migrations ({ApplicationContext})...", Constants.AppName);
     await app.ApplyMigrationsAndSeedData();
